0d802ee4f50eb62161804af8d34f0b7c3c3209a9
alisherodilov005 <alisherodilov741@gmail.com>
2024-02-21 15:44:06 +0500

    delete confirm

--- resources/views/components/delete-button.blade.php
+++ resources/views/components/delete-button.blade.php
@@ -1,8 +1,14 @@
 @props(['route'])
 
-    <form action="{{ $route }}" method="POST">
-        @csrf
-        @method('DELETE')
-        <button class="btn btn-danger" type="submit"><i class=" ri-delete-bin-line"></i></button>
-    </form>
-
+<form action="{{ $route }}" method="POST">
+    @csrf
+    @method('DELETE')
+    <button class="btn btn-danger" type="button" onclick="howalert(this)"><i class=" ri-delete-bin-line"></i></button>
+</form>
+<script>
+    function howalert(button){
+        if(confirm("O'chirmoqchimisz")){
+            button.parentElement.submit()
+        }
+    }
+</script>
